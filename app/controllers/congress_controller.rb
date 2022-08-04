43e46a5203f680ec32150e29bb8f2496689f5d27
Kat Brandt <joyfulkat11@gmail.com>
2022-08-04 11:10:23 -0600

    Adds connection for api call.

--- app/controllers/congress_controller.rb
+++ app/controllers/congress_controller.rb
@@ -1,2 +1,14 @@
 class CongressController < ApplicationController
+  def index
+    # state = params[:state]
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-KEY"] = Rails.application.credentials.dig(:PROPUBLICA_API_KEY)
+    end
+
+    response = conn.get("/congress/v1/members/house/CO/current.json")
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    @members = data[:results]
+    render 'congress/index'
+  end
 end
